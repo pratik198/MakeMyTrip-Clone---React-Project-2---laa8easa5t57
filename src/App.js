878c6753084f5c14f355eb00bcf9310248b719a9
pratik <routpratik09@gmail.com>
2023-11-08 21:10:02 +0530

    start header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header1 from "./components/Header1/Header1.js";
+
+function App() {
+  return (
+  <div className="App">
+    <Header1/>
+  </div>
+  )
+  }
+
+export default App;
---
+++ src/components/Header1/Header1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Header1.css'
+
+function Header1() {
+  return (
+    <div className='header1'>Header1</div>
+  )
+}
+
+export default Header1
