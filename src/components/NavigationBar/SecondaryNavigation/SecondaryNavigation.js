516f717e8abbb1d1ebccb5068701245fbba4b377
Arun pratik <routpratik09@gmail.com>
2024-04-18 14:08:44 +0530

    changes

--- src/components/NavigationBar/SecondaryNavigation/SecondaryNavigation.js
+++ src/components/NavigationBar/SecondaryNavigation/SecondaryNavigation.js
@@ -1,30 +1,154 @@
+// import React, { useState } from "react";
+// import "./SecondaryNavigation.css";
+// import LoginSignUpButton from "../../../Assets/SecondaryNavLoginSignUp.png";
+// import MakeMyTripLogo from "../../../Assets/mmtlogo2.png";
+// import { useNavigate } from "react-router";
+// import Login from "../../LoginSignUp/Login";
+
+// const SecondaryNavigation = () => {
+//   const navigate = useNavigate();
+
+//   const handleNavigateFlights = () => {
+//     navigate("/");
+//   };
+//   const handleNavigateHotels = () => {
+//     navigate("/hotelsearch");
+//   };
+//   const handleNavigateCommingSoon = () => {
+//     navigate("/commingsoonsearch");
+//   };
+
+//   const handleNavigateTrains = () => {
+//     navigate("/trainsearch");
+//   };
+
+//   const handleNavigateBuses = () => {
+//     navigate("/bussearch");
+//   };
+//   const [loginOpen, setLoginOpen] = useState(false);
+//   const [, forceUpdate] = useState();
+
+//   const handleLoginOpen = () => {
+//     setLoginOpen((prevLoginOpen) => !prevLoginOpen);
+//   };
+
+//   const handleLogout = () => {
+//     localStorage.removeItem("jwtToken");
+//     forceUpdate((prev) => !prev);
+//   };
+//   const jwt = localStorage.getItem("jwtToken");
+//   return (
+//     <div className="SecondaryNvigation__Parent_Container">
+//       <div className="SecondaryNvigation__Child_Container">
+//         <div className="SecondaryNvigation__Navigation_Container">
+//           <img
+//             onClick={handleNavigateFlights}
+//             className="SecondaryNvigation__Navigation_Container_makeMytrip_logo"
+//             src={MakeMyTripLogo}
+//           />
+//           <ul>
+//             <li onClick={handleNavigateFlights}>
+//               <div className="SecondaryNvigation__Navigation_FlightsImg_div">
+//                 <img
+//                   className="SecondaryNvigation__Navigation_FlightsImg"
+//                   src="https://imgak.mmtcdn.com/pwa_v3/pwa_commons_assets/desktop/B2CHeaderSprite.png"
+//                 />
+//               </div>
+//               <span>Flights</span>
+//             </li>
+//             <li onClick={handleNavigateHotels}>
+//               <div className="SecondaryNvigation__Navigation_HotelsImg_div">
+//                 <img
+//                   className="SecondaryNvigation__Navigation_HotelsImg"
+//                   src="https://imgak.mmtcdn.com/pwa_v3/pwa_commons_assets/desktop/B2CHeaderSprite.png"
+//                 />
+//               </div>
+//               <span>Hotels</span>
+//             </li>
+//             <li onClick={handleNavigateTrains}>
+//               <div className="SecondaryNvigation__Navigation_TrainsImg_div">
+//                 <img
+//                   className="SecondaryNvigation_trian_Img"
+//                   src="https://imgak.mmtcdn.com/pwa_v3/pwa_commons_assets/desktop/B2CHeaderSprite.png"
+//                 />
+//               </div>
+//               <span>Trains</span>
+//             </li>
+//             <li onClick={handleNavigateBuses}>
+//               <div className="SecondaryNvigation__Navigation_BusesImg_div">
+//                 <img
+//                   className="SecondaryNvigation_Buses_Img"
+//                   src="https://imgak.mmtcdn.com/pwa_v3/pwa_commons_assets/desktop/B2CHeaderSprite.png"
+//                 />
+//               </div>
+//               <span>Buses</span>
+//             </li>
+//           </ul>
+//         </div>
+//         {jwt === null ? (
+//           <img
+//             onClick={handleLoginOpen}
+//             className="w-[120px] cursor-pointer"
+//             src={LoginSignUpButton}
+//             alt="LoginSignUpButton"
+//           />
+//         ) : (
+//           <div
+//             style={{
+//               width: "180px",
+//               display: "flex",
+//               alignItems: "center",
+//               cursor: "pointer",
+//             }}
+//           >
+//             <div
+//               style={{
+//                 backgroundColor: "lightgreen",
+//                 height: "30px",
+//                 width: "30px",
+//                 borderRadius: "50%",
+//                 marginLeft: "10px",
+//                 display: "flex",
+//                 alignItems: "center",
+//                 justifyContent: "center",
+//               }}
+//             >
+//               {" "}
+//               <p style={{ color: "white", fontWeight: "bold" }}>M</p>
+//             </div>
+//             <p
+//               style={{
+//                 fontSize: "12px",
+//                 fontWeight: "bold",
+//                 marginLeft: "10px",
+//               }}
+//               onClick={handleLogout}
+//             >
+//               Hi {localStorage.getItem("name")}
+//             </p>
+//           </div>
+//         )}
+//       </div>
+//       {loginOpen && <Login onClose={handleLoginOpen} />}
+//     </div>
+//   );
+// };
+
+// export default SecondaryNavigation;
 import React, { useState } from "react";
 import "./SecondaryNavigation.css";
 import LoginSignUpButton from "../../../Assets/SecondaryNavLoginSignUp.png";
 import MakeMyTripLogo from "../../../Assets/mmtlogo2.png";
-import { useNavigate } from "react-router";
 import Login from "../../LoginSignUp/Login";
 
 const SecondaryNavigation = () => {
-  const navigate = useNavigate();
-
-  const handleNavigateFlights = () => {
-    navigate("/");
-  };
-  const handleNavigateHotels = () => {
-    navigate("/hotelsearch");
-  };
-  const handleNavigateCommingSoon = () => {
-    navigate("/commingsoonsearch");
+  const handleScrollDown = () => {
+    window.scrollBy({
+      top: 300,
+      behavior: "smooth",
+    });
   };
 
-  const handleNavigateTrains = () => {
-    navigate("/trainsearch");
-  };
-
-  const handleNavigateBuses = () => {
-    navigate("/bussearch");
-  };
   const [loginOpen, setLoginOpen] = useState(false);
   const [, forceUpdate] = useState();
 
@@ -36,18 +160,20 @@ const SecondaryNavigation = () => {
     localStorage.removeItem("jwtToken");
     forceUpdate((prev) => !prev);
   };
+
   const jwt = localStorage.getItem("jwtToken");
+
   return (
     <div className="SecondaryNvigation__Parent_Container">
       <div className="SecondaryNvigation__Child_Container">
         <div className="SecondaryNvigation__Navigation_Container">
           <img
-            onClick={handleNavigateFlights}
+            onClick={handleScrollDown}
             className="SecondaryNvigation__Navigation_Container_makeMytrip_logo"
             src={MakeMyTripLogo}
           />
           <ul>
-            <li onClick={handleNavigateFlights}>
+            <li onClick={handleScrollDown}>
               <div className="SecondaryNvigation__Navigation_FlightsImg_div">
                 <img
                   className="SecondaryNvigation__Navigation_FlightsImg"
@@ -56,7 +182,7 @@ const SecondaryNavigation = () => {
               </div>
               <span>Flights</span>
             </li>
-            <li onClick={handleNavigateHotels}>
+            <li onClick={handleScrollDown}>
               <div className="SecondaryNvigation__Navigation_HotelsImg_div">
                 <img
                   className="SecondaryNvigation__Navigation_HotelsImg"
@@ -65,7 +191,7 @@ const SecondaryNavigation = () => {
               </div>
               <span>Hotels</span>
             </li>
-            <li onClick={handleNavigateTrains}>
+            <li onClick={handleScrollDown}>
               <div className="SecondaryNvigation__Navigation_TrainsImg_div">
                 <img
                   className="SecondaryNvigation_trian_Img"
@@ -74,7 +200,7 @@ const SecondaryNavigation = () => {
               </div>
               <span>Trains</span>
             </li>
-            <li onClick={handleNavigateBuses}>
+            <li onClick={handleScrollDown}>
               <div className="SecondaryNvigation__Navigation_BusesImg_div">
                 <img
                   className="SecondaryNvigation_Buses_Img"
